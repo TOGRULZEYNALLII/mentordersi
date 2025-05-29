1f916068c1bc8f39da75f836a48424d5fb7e1387
Your Name <you@example.com>
2025-05-29 22:32:24 +0400

    css problemi

---
+++ slider/script.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('#button');
+const images = document.querySelectorAll('img');
+let currentIndex = 0;
+function nextslide(){
+    images.forEach((img, index) => {
+        img.style.transform = `translateX(${(index - currentIndex) * 100}px)`;
+    });
+    currentIndex++;
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+    
+}
+
+button.addEventListener('click', nextslide);
